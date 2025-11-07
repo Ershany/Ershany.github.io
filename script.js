a2ace74fc556e571d9816126d9b23673a2cc307c
Brady Jessup <bradyjessup@hotmail.com>
2025-11-07 16:00:17 -0500

    Attempted to fix mobile bug

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ const menuToggle = document.getElementById('mobile-menu');
 const navLinks = document.querySelector('.nav-links');
 
 menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+    navLinks.classList.toggle('show');
+});
+
+// Close menu when a nav link is clicked
+document.querySelectorAll('.nav-links a').forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('show');
+  });
 });
